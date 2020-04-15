337afed6fccaf1b810da4aeb072ad31fbbb56661
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2020-04-16 01:07:11 +0300

    Add hello-world

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,8 @@
+// Package greeting implements
+// HelloWorld() function that returns "hello, world!"
+package greeting
+
+// HelloWorld returns "hello, world!"
+func HelloWorld() string {
+	return "hello, world!"
+}
