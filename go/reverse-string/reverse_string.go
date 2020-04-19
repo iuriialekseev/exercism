535946f364d9a36c3bfac36f6f03a2a5d853d283
Yuriy Alekseyev <dev.yuriy.a@gmail.com>
2020-04-19 18:17:22 +0300

    Add reverse-string

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,11 @@
+// Package reverse implements utilities for reverse strings
+package reverse
+
+// Reverse accepts string and reverts it
+func Reverse(input string) string {
+	runes := []rune(input)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
